1a18b9bc5a41e4da02657a655da9310ebcf52844
Santiago <shpotes3@gmail.com>
2020-12-07 23:14:16 -0500

    add notebook utils

---
+++ notebooks/utils.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+from PIL import Image 
+import textwrap
+import os
+
+def display_images(
+    images: [Image.Image], 
+    columns=5, width=20, height=8, max_images=15, 
+    label_wrap_length=50, label_font_size=8):
+
+    if not images:
+        print("No images to display.")
+        return 
+
+    if len(images) > max_images:
+        print(f"Showing {max_images} images of {len(images)}:")
+        images=images[0:max_images]
+
+    height = max(height, int(len(images)/columns) * height)
+    plt.figure(figsize=(width, height))
+    for i, image in enumerate(images):
+
+        plt.subplot(len(images) / columns + 1, columns, i + 1)
+        plt.imshow(image)
+        plt.axis('off')
+
+        if hasattr(image, 'filename'):
+            title=image.filename
+            if title.endswith("/"): title = title[0:-1]
+            title=os.path.basename(title)
+            title=textwrap.wrap(title, label_wrap_length)
+            title="\n".join(title)
